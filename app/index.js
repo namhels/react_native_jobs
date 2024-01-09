cf57c9997799c4abaed96611243e261183860047
Serhii Lekhman <8.lsv.888@gmail.com>
2024-01-09 11:23:13 +0100

    add started assets, components, constants, styles

--- app/index.js
+++ app/index.js
@@ -1,10 +1,26 @@
-import { View, Text } from 'react-native';
+import { useState } from "react";
+import { SafeAreaView, ScrollView, View } from "react-native";
+import { Stack, useRouter } from "expo-router";
+
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
 
 const Home = () => {
+  const router = useRouter()
+
 	return (
-		<View>
-			<Text>Yahooooooo8:)</Text>
-		</View>
+		<SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+			<Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+        }}
+      />
+		</SafeAreaView>
 	)
 }
 
