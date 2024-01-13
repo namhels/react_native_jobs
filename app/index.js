52a56ff2d646a19a0d78977bfedb258322cc5468
Serhii Lekhman <8.lsv.888@gmail.com>
2024-01-13 23:03:42 +0100

    add ScrollView

--- app/index.js
+++ app/index.js
@@ -11,41 +11,38 @@ import {
 } from "../components";
 
 const Home = () => {
-  const router = useRouter()
+  const router = useRouter();
 
-	return (
-		<SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
-			<Stack.Screen
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
         options={{
           headerStyle: { backgroundColor: COLORS.lightWhite },
           headerShadowVisible: false,
           headerLeft: () => (
-            <ScreenHeaderBtn iconUrl={icons.menu} dimension='60%' />
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
           ),
           headerRight: () => (
-            <ScreenHeaderBtn iconUrl={images.profile} dimension='100%' />
+            <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
           ),
           headerTitle: "",
         }}
       />
 
-<ScrollView showsVerticalScrollIndicator={false}>
+      <ScrollView showsVerticalScrollIndicator={false}>
         <View
           style={{
             flex: 1,
             padding: SIZES.medium,
           }}
         >
-          <Welcome
-            
-          />
-
+          <Welcome />
           <Popularjobs />
           <Nearbyjobs />
         </View>
       </ScrollView>
-		</SafeAreaView>
-	)
-}
+    </SafeAreaView>
+  );
+};
 
 export default Home;
