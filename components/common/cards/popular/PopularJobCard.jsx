d590f2fd846c56d0fee3521f4407eb8e5daceb42
Serhii Lekhman <8.lsv.888@gmail.com>
2024-01-21 12:35:49 +0100

    update PopularjobCard.js

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import styles from "./popularjobcard.style";
 
-import styles from './popularjobcard.style'
+import styles from "./popularjobcard.style";
 
 const PopularJobCard = () => {
   return (
-    <View>
-      <Text>PopularJobCard</Text>
-    </View>
-  )
-}
+    <TouchableOpacity
+      style={styles.container(selectedJob, item)}
+      onPress={() => handleCardPress(item)}
+    ></TouchableOpacity>
+  );
+};
 
-export default PopularJobCard
+export default PopularJobCard;
