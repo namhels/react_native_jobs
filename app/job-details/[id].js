310ab4027b49f7be2d45b5a79d8375ddf07acd19
Serhii Lekhman <8.lsv.888@gmail.com>
2024-02-02 18:54:07 +0100

    update job-details/[id].js

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -64,7 +64,12 @@ const JobDetails = () => {
             <Text>No data available</Text>
           ) : (
             <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
-              <Company />
+              <Company
+                companyLogo={data[0].employer_logo}
+                jobTitle={data[0].job_title}
+                companyName={data[0].employer_name}
+                location={data[0].job_country}
+              />
 
               <JobTabs />
 
