71dbb983ecce407a32221c2fa193b73e8c393112
Serhii Lekhman <8.lsv.888@gmail.com>
2024-02-11 09:44:54 +0100

    update app/index.js

--- app/index.js
+++ app/index.js
@@ -12,6 +12,7 @@ import {
 
 const Home = () => {
   const router = useRouter();
+  const [searchTerm, setSearchTerm] = useState("");
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
